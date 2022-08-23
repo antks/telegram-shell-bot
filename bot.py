df09182270f07a5c288f900b55360632931f888f
antks <111529474+antks@users.noreply.github.com>
2022-08-23 13:21:24 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,13 +29,7 @@ __tasks = set()
 def validate_settings():
     if -999999 not in settings.ENABLED_USERS:
         return
-    if not (settings.CMD_WHITE_LIST or settings.ONLY_SHORTCUT_CMD):
-        raise Exception(
-            "It a public bot. "
-            "Public bot is not safe, dont's use root to run this bot. "
-            "You must add settings `CMD_WHITE_LIST` or `ONLY_SHORTCUT_CMD=True` for a public bot"
-        )
-
+    
 
 def restricted(func):
     @wraps(func)
